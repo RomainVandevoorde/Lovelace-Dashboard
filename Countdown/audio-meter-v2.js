4dfae5c19d4e76fce9661ce3d600041c95b797f1
Romain Vandevoorde <romain.vandevoorde.dev@gmail.com>
2018-06-18 17:08:06 +0200

    new prototype

---
+++ Countdown/audio-meter-v2.js
@@ -0,0 +1,193 @@
+
+const circle = document.getElementsByTagName('circle')[0];
+const number = document.getElementsByTagName('p')[0];
+
+// let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+// let analyser = audioCtx.createAnalyser();
+//
+//
+//
+// if (navigator.mediaDevices.getUserMedia) {
+//    console.log('getUserMedia supported.');
+//    let constraints = {audio: true};
+//    navigator.mediaDevices.getUserMedia (constraints)
+//       .then(
+//         function(stream) {
+//            source = audioCtx.createMediaStreamSource(stream);
+//            source.connect(analyser);
+//
+//         	 display();
+//       })
+//       .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+// } else {
+//    alert('getUserMedia not supported on your browser!');
+// }
+//
+//
+// analyser.fftSize = 32;
+// var bufferLength = analyser.frequencyBinCount;
+// var dataArray = new Uint8Array(bufferLength);
+// console.log(bufferLength);
+// max = 0;
+// min = 0;
+//
+// function display() {
+//
+//   analyser.getByteFrequencyData(dataArray);
+//   // dataArray.fill(128);
+//   // console.log(dataArray);
+//
+//   let sum = 0;
+//
+//   for(var i = 0; i < bufferLength; i++) {
+//     // x = (dataArray[i]-128)*1.0/128.0;
+//     // let x = dataArray[i];
+//     let x = (dataArray[i]*1.0)/256.0;
+//     sum += x*x;
+//     // sum += x;
+//   }
+//
+//   let rms = Math.sqrt(sum/bufferLength);
+//   if(rms > max) max = rms;
+//   // let av = sum/bufferLength;
+//
+//   // console.log(rms);
+//   //
+//   let nb = Math.floor(rms*100);
+//
+//   number.innerHTML = nb+"<br>"+min+"<br>"+max;
+//   circle.setAttribute('r', rms*400);
+//
+//   window.setTimeout(display, 50);
+//     // requestAnimationFrame(display);
+// }
+
+class VolumeMeter {
+  constructor() {
+    // Settings
+    this.nbDataRecords = 40;
+
+    this.lastRMS = 0;
+    this.allTimeMin = null;
+    this.allTimeMax = null;
+    this.data = [];
+    this.avg = null;
+    this.scaledAvg = null;
+    this.color = "rgb(0,0,0)";
+
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    this.analyser = this.audioCtx.createAnalyser();
+    this.gotStream = false;
+    this.streamStart = 0;
+
+    this.analyser.fftSize = 128;
+    this.bufferLength = this.analyser.frequencyBinCount;
+    this.dataArray = new Uint8Array(this.bufferLength);
+
+    this.checkSupport();
+  }
+
+  checkSupport() {
+    if (navigator.mediaDevices.getUserMedia) {
+      console.log('getUserMedia supported.');
+      this.support = true;
+    } else {
+      alert('getUserMedia not supported');
+      this.support = false;
+    }
+  }
+
+  getMediaStream() {
+
+    if(!this.support) return false;
+
+    navigator.mediaDevices.getUserMedia({audio:true})
+    .then(this.bindStream.bind(this))
+    .catch( function(err) { console.log('The following gUM error occured: ' + err);});
+  }
+
+  bindStream(stream) {
+    this.source = this.audioCtx.createMediaStreamSource(stream);
+    this.source.connect(this.analyser);
+    this.gotStream = true;
+    this.streamStart = Date.now();
+  }
+
+  main() {
+    this.calcRMS();
+    let now = Date.now();
+    if(now - vm.streamStart > 2000 && this.gotStream) {
+      this.pushData();
+      this.avgData();
+    }
+    if(this.data.length > 39) {
+      if(!this.allTimeMax || this.avg > this.allTimeMax) this.allTimeMax = this.avg;
+      if(!this.allTimeMin || this.avg < this.allTimeMin) this.allTimeMin = this.avg;
+    }
+    if(this.allTimeMin && this.allTimeMax && this.allTimeMax - this.allTimeMin > 0) {
+      this.calcScaledAvg();
+      this.calcColor();
+    }
+  }
+
+  calcRMS() {
+    this.analyser.getByteFrequencyData(this.dataArray);
+
+    let sum = 0;
+
+    for(var i = 0; i < this.bufferLength; i++) {
+      let x = (this.dataArray[i]*1.0)/256.0;
+      sum += x*x;
+    }
+
+    this.lastRMS = Math.sqrt(sum/this.bufferLength);
+  }
+
+  pushData() {
+    if(this.data.length >= this.nbDataRecords) this.data.shift();
+    this.data.push(this.lastRMS);
+  }
+
+  avgData() {
+    let len = this.data.length;
+    let total = 0;
+    for(let i = 0; i < len; i++) {
+      total += this.data[i];
+    }
+    this.avg = total/len;
+  }
+
+  calcScaledAvg() {
+    this.scaledAvg = 100*(this.avg - this.allTimeMin)/(this.allTimeMax - this.allTimeMin);
+  }
+
+  calcColor() {
+    if(this.scaledAvg < 0) this.color = "rgb(0,0,0)";
+    else if(this.scaledAvg <= 50) {
+      let redLvl = (this.scaledAvg/50)*200;
+      this.color = "rgb("+redLvl+",200,0)";
+    }
+    else if(this.scaledAvg <=100) {
+      let greenLvl = 255 - (((this.scaledAvg-50)/50)*255);
+      this.color = "rgb(200,"+greenLvl+",0)";
+    }
+    else this.color = "rgb(255,0,0)";
+  }
+}
+
+let vm = new VolumeMeter();
+vm.getMediaStream();
+
+
+function vmLoop(vm) {
+  vm.main();
+
+  if(vm.scaledAvg !== null) {
+    circle.setAttribute('r', (vm.scaledAvg*3)+100);
+    circle.setAttribute('fill', vm.color);
+    number.innerHTML = Math.floor(vm.scaledAvg);
+  }
+  window.setTimeout(vmLoop, 50, vm);
+}
+
+vmLoop(vm);
